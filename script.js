f1e3ed4d3d457e363792cb8debbb48e1d243a408
Izu Eneh <ehen@ualberta.ca>
2022-01-10 21:38:49 -0700

    grid can be drawn on

--- script.js
+++ script.js
@@ -9,4 +9,9 @@ for (let i = 0; i < (16*16); i++) {
     gridContainer.appendChild(gridSquare);
 }
 
+const gridSquares = document.querySelectorAll('.grid-square'); // change class to id; 
+gridSquares.forEach(square => square.addEventListener('mouseenter', addHoverEffect))
 
+function addHoverEffect(e) {
+    this.classList.add('black-background');
+}
