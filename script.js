9f9bfd386880811fc61db4dab5bc6c175bef1920
Izu Eneh <ehen@ualberta.ca>
2022-01-10 21:45:59 -0700

    clearing grid works

--- script.js
+++ script.js
@@ -5,13 +5,20 @@ for (let i = 0; i < (16*16); i++) {
     let gridSquare = document.createElement('div');
     gridSquare.style.height = `${gridSideLength}px`;
     gridSquare.style.width = `${gridSideLength}px`;
-    gridSquare.classList.add('grid-square');
+    gridSquare.id = 'grid-square'
     gridContainer.appendChild(gridSquare);
 }
 
-const gridSquares = document.querySelectorAll('.grid-square'); // change class to id; 
+const gridSquares = document.querySelectorAll('#grid-square');  
 gridSquares.forEach(square => square.addEventListener('mouseenter', addHoverEffect))
+
+const clearGridButton = document.querySelector('button');
+clearGridButton.addEventListener('click', clearGrid)
 
 function addHoverEffect(e) {
     this.classList.add('black-background');
+}
+
+function clearGrid(e) {
+    gridSquares.forEach(square => square.classList.remove('black-background'));
 }
