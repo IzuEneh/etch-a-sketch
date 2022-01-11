f3235c04bfd7949ec124960befd408534a5ec9bb
Izu Eneh <ehen@ualberta.ca>
2022-01-10 21:59:47 -0700

    completed basic tasks

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 const gridContainer = document.querySelector('#grid-container');
-const gridSideLength = gridContainer.clientWidth / 16; 
-
-for (let i = 0; i < (16*16); i++) {
-    let gridSquare = document.createElement('div');
-    gridSquare.style.height = `${gridSideLength}px`;
-    gridSquare.style.width = `${gridSideLength}px`;
-    gridSquare.id = 'grid-square'
-    gridContainer.appendChild(gridSquare);
-}
-
-const gridSquares = document.querySelectorAll('#grid-square');  
-gridSquares.forEach(square => square.addEventListener('mouseenter', addHoverEffect))
+fillGridWithSquares(16);
+setHoverEffectOnGrid();
 
 const clearGridButton = document.querySelector('button');
 clearGridButton.addEventListener('click', clearGrid)
 
-function addHoverEffect(e) {
-    this.classList.add('black-background');
+function fillGridWithSquares(numSquares) {
+    const gridSideLength = gridContainer.clientWidth / numSquares; 
+    
+    for (let i = 0; i < (numSquares*numSquares); i++) {
+        let gridSquare = document.createElement('div');
+        gridSquare.style.height = `${gridSideLength}px`;
+        gridSquare.style.width = `${gridSideLength}px`;
+        gridSquare.id = 'grid-square'
+        gridContainer.appendChild(gridSquare);
+    }
 }
 
 function clearGrid(e) {
-    gridSquares.forEach(square => square.classList.remove('black-background'));
+    gridContainer.replaceChildren();
+    numSquares = parseInt(prompt('How many squares would you like?', 16));
+    fillGridWithSquares(numSquares);
+    setHoverEffectOnGrid();
+}
+
+function setHoverEffectOnGrid() {
+    const gridSquares = document.querySelectorAll('#grid-square');  
+    gridSquares.forEach(square => square.addEventListener('mouseenter', addHoverEffect))
+}
+
+function addHoverEffect(e) {
+    this.classList.add('black-background');
 }
